8bf4e1e6f05b9a6184848d1630322bd9f276012d
Tom <ya62z562000@yahoo.com.tw>
2019-04-14 20:26:45 +0800

    Add route

--- app.js
+++ app.js
@@ -20,14 +20,15 @@ db.once('open', () => {
 const restaurants = require('./models/restaurant')
 
 app.use(express.static('public'))
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//取得所有餐廳
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantsList.results })
 })
 
+//取得餐廳詳細資料
 app.get('/restaurants/:restaurants_id', (req, res) => {
   const restaurant = restaurantsList.results.filter(function (restaurant) {
     return restaurant.id == req.params.restaurants_id
@@ -35,6 +36,32 @@ app.get('/restaurants/:restaurants_id', (req, res) => {
   res.render('show', { restaurant: restaurant[0] })
 })
 
+//新增餐廳的頁面
+app.get('/restaurants/new', (req, res) => {
+  res.render('新增餐廳')
+})
+
+//新增餐廳
+app.post('/restaurants', (req, res) => {
+  res.render('新增一筆餐廳')
+})
+
+//修改餐廳資訊的頁面
+app.get('/restaurants/:restaurants_id/edit', (req, res) => {
+  res.render('修改餐廳')
+})
+
+//修改餐廳
+app.post('/restaurants/:restaurants_id', (req, res) => {
+  res.render('/修改餐廳資訊')
+})
+
+//刪除餐廳資訊
+app.post('/restaurants/:restaurants_id/delete', (req, res) => {
+  res.render('刪除餐廳')
+})
+
+//搜尋餐廳
 app.get('/search', (req, res) => {
   console.log('query.keyword', req.query.keyword)
   const restaurants = restaurantsList.results.filter(function (restaurants) {
